bec2b00234cb579954a053797b32e5168dbf4e0d
abc <abc@example.com>
2020-11-28 20:55:18 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const gotohome = () => {
+    window.location.href = "index.html";
+}
+
+const gotoproject = () => {
+    window.location.href = "project.html";
+}
+
+const gotocontact = () => {
+    window.location.href = "Contact.html";
+}
